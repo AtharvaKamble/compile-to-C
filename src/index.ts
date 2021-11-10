5e4f61a70cf19ae36f208e3d8aab27258373a68c
AtharvaKamble <atharvadkamble00@gmail.com>
2021-11-10 19:33:27 +0530

    feat: runner code for sane-testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import Lexer from './lib/lexer';
+import { TokenCode } from './lib/lexer/token';
 
-console.log(Lexer);
+const lex = new Lexer('==!=>>// This is a comment!  \n  =<=<');
+let token = lex.getToken();
+
+while (token.code !== TokenCode.EOF) {
+  console.log(token);
+  token = lex.getToken();
+}
