35e463b082fd4a1e1cfd3d1f6b354bf171127558
AtharvaKamble <atharvadkamble00@gmail.com>
2021-11-10 19:34:03 +0530

    feat: add global token-code mapping

---
+++ src/lib/lexer/token/index.ts
@@ -0,0 +1,42 @@
+import { TypeToken } from '@compiler/types';
+
+export class Token implements TypeToken {
+  text: string;
+  code: number;
+
+  constructor(text: string, code: number) {
+    this.text = text;
+    this.code = code;
+  }
+}
+
+export enum TokenCode {
+  EOF = -1,
+  NEWLINE = 0,
+  NUMBER = 1,
+  IDENT = 2,
+  STRING = 3,
+  LABEL = 101,
+  GOTO = 102,
+  PRINT = 103,
+  INPUT = 104,
+  LET = 105,
+  IF = 106,
+  THEN = 107,
+  ENDIF = 108,
+  WHILE = 109,
+  REPEAT = 110,
+  ENDWHILE = 111,
+  EQ = 201,
+  PLUS = 202,
+  MINUS = 203,
+  ASTERISK = 204,
+  SLASH = 205,
+  EQEQ = 206,
+  NOTEQ = 207,
+  LT = 208,
+  LTEQ = 209,
+  GT = 210,
+  GTEQ = 211,
+  COMMENT = 212,
+}
